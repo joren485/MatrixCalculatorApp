715ff9e0408c66792c3d8fd57d2677d0207372a1
Marco Hernandez <Marco.Hernandez@hotmail.nl>
2016-06-08 16:50:37 +0200

    Extra uitleg toegevoegd.

--- app/src/main/java/com/asserttrue/matrixcalculator/view/computationsTab/FormulaInfoActivity.java
+++ app/src/main/java/com/asserttrue/matrixcalculator/view/computationsTab/FormulaInfoActivity.java
@@ -17,8 +17,13 @@ public class FormulaInfoActivity extends AppCompatActivity {
     private static final String DETERMINANT =
             "The determinant is a function from the set of matrices to the (real, rational or complex) numbers. It reveals several important qualities of the matrix all at once. Most importantly, a square matrix has determinant 0 if and only if it is not invertible. Also, a determinant of a 3-dimensional matrix gives the factor with which it rescales the unit cube.\n\nThe determinant is usually defined recursively with Laplace expansion. Although this definition provides an explicit way to compute the determinant, this is rarely used; there are faster methods available.\n\nThe alogirthm used here applies row reduction make the matrix upper-triangular, because the determinant of an upper-triangular matrix is its trace (in this case 1, because the diangonal is all ones). Adding one row to another does not change the value of the determinant since the determinant is an n-linear function on the rows (and columns) of a matrix. We can divide a row by any nonzero number, if we multiply the result by that number. Swapping two rows changes the sign of the outcome.";
     private static final String KERNEL =
-            "The kernel of a matrix is the set of vectors that it maps onto one. Since this is vector space, it is always possible to find a basis for it.\n\nSince the kernel of a matrix does not change under application of elementary row operations, we can first bring the matrix into reduced row echelon form. The resulting system of linear equations is much simplified.";
-
+            "The kernel of a matrix is the set of vectors that it maps onto one. Since this is vector space, it is always possible to find a basis for it.\n\nSince the kernel of a matrix does not change under application of elementary row operations, we can first bring the matrix into reduced row echelon form. The resulting system of linear equations is much simplified. We find it by using the fact that if we add -1 to the free columns along the diagonal, and insert zeroes into them for eaach previous free column, then they form a basis for the kernel.\n\nIt is of course possible to write out the system of equations manually, and this basis is far from unique.";
+    private static final String RREF =
+            "The reduced row echelon form is a simplified form of a matrix, in which the solution to a system of linear equations is easier to read off.\n\nAs the name implies, the row echelon form is reached by only using row operations. The row operations correspond to adding multiples of one equation to another. The solution space for the matrix in the end is the same as the one of the matrix one started with.";
+    private static final String SCALAR_MULT =
+            "Scalar multiplication of a matrix or vector is done by multiplying each individual cell with that number. It distributes over scalar multiplication.";
+    private static final String EXPONENTIATION =
+            "The whole-number exponent of matrix can be computed simply by multiplying it by itself that number of times. Note that it does not matter whether you multiply on the right or left.\n\nAlthough real exponentiation for matrices is also defined, it requires somewhat advance computation.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +57,17 @@ public class FormulaInfoActivity extends AppCompatActivity {
                 title.setText("Addition");
                 body.setText(ADDITION);
                 break;
+            case("rref"):
+                title.setText("Row Echelon Form");
+                body.setText(RREF);
+                break;
+            case("exponent"):
+                title.setText("Exponentiation");
+                body.setText(EXPONENTIATION);
+                break;
+            case("scalar mult"):
+                title.setText("Scalar Multiplication");
+                body.setText(SCALAR_MULT);
             default:
                 title.setVisibility(View.GONE);
                 body.setVisibility(View.GONE);
