6b19ebceabdc66af5fb6e65339760c1a0df5e6b9
Gijs Hendriksen <gijs@hendrxx.nl>
2016-06-08 23:04:36 +0200

    Bugfixes

--- app/src/main/java/com/asserttrue/matrixcalculator/view/storedMatricesTab/EditMatrixActivity.java
+++ app/src/main/java/com/asserttrue/matrixcalculator/view/storedMatricesTab/EditMatrixActivity.java
@@ -48,6 +48,7 @@ public class EditMatrixActivity extends AppCompatActivity {
 
         final MathTextButton zeroButton = (MathTextButton) findViewById(R.id.zeroMatrixButton);
         final MathTextButton identityButton = (MathTextButton) findViewById(R.id.identityMatrixButton);
+        final CheckBox augmentedCheckbox = (CheckBox) findViewById(R.id.augmentedCheckbox);
 
         saveMatrixBox = (CheckBox) findViewById(R.id.saveMatrixCheckbox);
         finishEditingButton = (Button) findViewById(R.id.finishEditingButton);
@@ -61,7 +62,6 @@ public class EditMatrixActivity extends AppCompatActivity {
         rowSpinner.setAdapter(adapter);
 
         grid = (EditMatrixGridView) findViewById(R.id.gridView);
-        grid.setNumColumns(2);
 
         EditMatrixSingleton matrixSettings = EditMatrixSingleton.getInstance();
 
@@ -85,6 +85,12 @@ public class EditMatrixActivity extends AppCompatActivity {
         if (matrixName.getText().toString().isEmpty() && saveMatrixBox.isChecked())
             finishEditingButton.setEnabled(false);
 
+        if (matrixSettings.editMatrix.getAugmentedColumnIndex() >= 0 && matrixSettings.editMatrix.getAugmentedColumnIndex() < matrixSettings.editMatrix.getNrColumns()) {
+            augmentedCheckbox.setChecked(true);
+        } else {
+            augmentedLineSeekbar.setVisibility(View.GONE);
+        }
+
         editMatrixAdapter = new EditMatrixAdapter(this, matrixSettings.editMatrix);
         grid.setAdapter(editMatrixAdapter);
 
@@ -105,6 +111,17 @@ public class EditMatrixActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+            }
+        });
+
+        augmentedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                augmentedLineSeekbar.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                if (!isChecked) {
+                    editMatrixAdapter.updateAugmentedIndex(-1);
+                    augmentedLineSeekbar.setProgress(0  );
+                }
             }
         });
 
